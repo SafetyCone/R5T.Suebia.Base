b3449fcc748a58ab670cf224580b62adc7a569fb
MinexAutomation <minex.automation@gmail.com>
2019-10-17 00:55:55 -0700

    Added Rivet Secrets directory and file path provider interfaces.

---
+++ source/R5T.Suebia.Base/Code/Interfaces/IRivetSecretsDirectoryPathProvider.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+
+namespace R5T.Suebia
+{
+    public interface IRivetSecretsDirectoryPathProvider
+    {
+        string GetRivetSecretsDirectoryPath();
+    }
+}
---
+++ source/R5T.Suebia.Base/Code/Interfaces/IRivetSecretsFilePathProvider.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+
+namespace R5T.Suebia
+{
+    public interface IRivetSecretsFilePathProvider
+    {
+        string GetRivetSecretsFilePath(string fileName);
+    }
+}
